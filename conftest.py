4de8a4c459a01ccf572046ed4c7bfc3e835a3d95
Aleksandr-TI <alexandrtkchnk@gmail.com>
2021-01-13 14:55:56 +0200

    additionl variables for test

--- conftest.py
+++ conftest.py
@@ -1,8 +1,31 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common import by
+from selenium.webdriver.support import select
+from selenium.webdriver.support import expected_conditions as conditition
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None, help="Choose language: en, ru, es, etc.")
+
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
-    yield browser
-    browser.quit()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox()
+    else:
+            raise pytest.UsageError("--browser_name should be chrome or firefox")
+            yield browser
+            browser.quit()
