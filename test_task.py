997d03b61841de8865990ff16b51898a52e10909
Aleksandr-TI <alexandrtkchnk@gmail.com>
2021-01-13 16:00:53 +0200

    update test summary

--- test_task.py
+++ test_task.py
@@ -1,4 +1,4 @@
-def test_btn_add_to_cart_exist_on_es(browser):
+def test_btn_name_related_to_locale_es(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     browser.get(link)
     Cart_btn = browser.find_element_by_css_selector(".btn-add-to-basket").text
