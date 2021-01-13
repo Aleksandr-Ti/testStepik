6b18683a8a4d10a6f2268f56340f92178080eff4
Aleksandr-TI <alexandrtkchnk@gmail.com>
2021-01-13 15:53:22 +0200

    fixes

--- test_task.py
+++ test_task.py
@@ -1,5 +1,5 @@
-def test_btn_add_to_cart_exist(self, browser):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    Cart_btn = browser.find_element_by_css_selector(".btn-add-to-basket")
-    cart_btn_language = cart_btn.text
-    assert cart_btn_language == "Añadir al carrito"
+def test_btn_add_to_cart_exist_on_es(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    browser.get(link)
+    Cart_btn = browser.find_element_by_css_selector(".btn-add-to-basket").text
+    assert Cart_btn == "Añadir al carrito"
