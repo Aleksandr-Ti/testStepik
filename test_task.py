88762b22ed7ba8ddf08ba5fa62e6e9099df073f4
Aleksandr-TI <alexandrtkchnk@gmail.com>
2021-01-13 15:59:17 +0200

    update assert

--- test_task.py
+++ test_task.py
@@ -2,4 +2,4 @@ def test_btn_add_to_cart_exist_on_es(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     browser.get(link)
     Cart_btn = browser.find_element_by_css_selector(".btn-add-to-basket").text
-    assert Cart_btn == "Añadir al carrito"
+    assert Cart_btn == "Añadir al carrito", f"get '{Cart_btn}' instead of 'Añadir al carrito'"
